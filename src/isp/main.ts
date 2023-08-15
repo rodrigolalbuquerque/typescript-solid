fa90d019b37670bc19159f7f560931f56eb393c3
Robrigado <robrigadodev@gmail.com>
2023-08-15 15:40:07 -0300

    aulas349+350 - ISP

--- src/isp/main.ts
+++ src/isp/main.ts
@@ -4,6 +4,7 @@ import { Persistency } from './services/persistency';
 import { Product } from './classes/product';
 import { ShoppingCart } from './classes/shopping-cart';
 import { TenPercentDiscount } from './classes/discounts';
+import { EnterpriseCustomer } from './classes/customer';
 
 // const noDiscount = new NoDiscount();
 // const fiftyPercentDiscount = new FiftyPercentDiscount();
@@ -11,7 +12,13 @@ const tenPercentDiscount = new TenPercentDiscount();
 const shoppingCart = new ShoppingCart(tenPercentDiscount);
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+// const pessoaFisica = new IndividualCustomer(
+//   'Rodrigo',
+//   'Lomar',
+//   '144.756.627-02',
+// );
+const empresa = new EnterpriseCustomer('fixdate', '11727821-1000/08');
+const order = new Order(shoppingCart, messaging, persistency, empresa);
 
 shoppingCart.addItem(new Product('carambola', 2.9));
 shoppingCart.addItem(new Product('balaustre', 49.9));
